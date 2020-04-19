d1ed950a2b78d58b45459a67b3d50d57ae0ee956
Davis <davisschenk@gmail.com>
2020-04-19 01:25:00 -0600

    Cleaned up code to use glob instead of regex and changed method or sorting modules

--- problems/__init__.py
+++ problems/__init__.py
@@ -1,9 +1,17 @@
 from os.path import dirname, basename, isfile, join
 import glob
 import re
+import fnmatch
 
-reg = re.compile(r"([0-9]+)_.+")
-# Dynamically Load all problems and make sure they are sorted by problem #
-modules = glob.glob(join(dirname(__file__), "*.py"))
-modules = {int(reg.search(module).group(1)): module for module in modules if not module.endswith('__init__.py')}
-__all__ = [basename(modules[f])[:-3] for f in sorted(modules) if isfile(modules[f])]
+modules = glob.glob(join(dirname(__file__), "[0-9]*.py"))
+
+
+def key(fn):
+    """
+    this returns the problem #
+    """
+    return int(basename(fn).split("_")[0])
+
+
+__all__ = [basename(module)[:-3] for module in sorted(modules, key=key)]
+
