402ebebf46cfa5a17e1f02c6f195ee73dfc96c91
Davis Schenkenberger <davisschenk@gmail.com>
2020-04-28 14:28:28 -0600

    Refactored to use new naming scheme and pathlib

--- problems/__init__.py
+++ problems/__init__.py
@@ -1,17 +1,9 @@
-from os.path import dirname, basename, isfile, join
-import glob
-import re
-import fnmatch
+from pathlib import Path
+from glob import glob
 
-modules = glob.glob(join(dirname(__file__), "[0-9]*.py"))
+# Find the parent directory of this file and recursively search for python files that follow problem naming scheme
+problems_dir = Path(__file__).parent
+modules = problems_dir.glob("**/p[0-9][0-9][0-9].py")
 
-
-def key(fn):
-    """
-    this returns the problem #
-    """
-    return int(basename(fn).split("_")[0])
-
-
-__all__ = [basename(module)[:-3] for module in sorted(modules, key=key)]
-
+# Remove the .py, sort by problem # and add to __all__
+__all__ = [module.stem for module in sorted(modules, key=lambda m: m.stem)]
