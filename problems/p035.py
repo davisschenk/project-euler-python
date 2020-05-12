f3cd151193404de8a29b6cc5c2117a87900bd53d
Davis Schenkenberger <35881002+davisschenk@users.noreply.github.com>
2020-05-11 20:16:58 -0600

    Problem 35

--- problems/p035.py
+++ problems/p035.py
@@ -5,22 +5,48 @@ from itertools import permutations
 
 class CircularPrimes(Problem):
     @Problem.solution()
-    def solution(self):
-        circular_primes = set()
+    def using_lookup_table(self):
+        primes = {k: False for k in sieve_of_eratosthenes(1_000_000)}
+
+        for prime in primes.keys():
+            if primes[prime]:
+                continue
+
+            if self.is_circular(prime, check_prime=lambda x: x in primes):
+                primes[prime] = True
+
+        return sum(primes.values())
+
+    @Problem.solution()
+    def using_list(self):
+        circular_list = []
 
         for prime in sieve_of_eratosthenes(1_000_000):
-            if prime in circular_primes:
+            if prime in circular_list:
                 continue
 
             if self.is_circular(prime):
-                circular_primes.add(prime)
+                circular_list.append(prime)
 
-        return len(circular_primes)
+        return len(circular_list)
+
+    @Problem.solution()
+    def using_set(self):
+        circular_list = set()
+
+        for prime in sieve_of_eratosthenes(1_000_000):
+            if prime in circular_list:
+                continue
+
+            if self.is_circular(prime):
+                circular_list.add(prime)
+
+        return len(circular_list)
 
     @classmethod
-    def is_circular(cls, n):
+    def is_circular(cls, n, check_prime=simple_is_prime):
         for rotation in cls.rotations(n):
-            if not simple_is_prime(rotation):
+            if not check_prime(rotation):
                 return False
 
         return True
