4f148ff39475f513d03ada973e4ef95c6323a33a
Davis Schenkenberger <davisschenk@gmail.com>
2020-10-08 17:44:07 -0600

    Rename function

--- problems/p029.py
+++ problems/p029.py
@@ -3,6 +3,6 @@ from problem import Problem
 
 class DistinctPowers(Problem):
     @Problem.solution()
-    def solution(self):
+    def brute_force(self):
         # Good ol fashion set comprehension
         return len({a ** b for a in range(2, 101) for b in range(2, 101)})
