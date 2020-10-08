463a6594e0793ae617ff02c9cea44e5eacf58d94
Davis Schenkenberger <davisschenk@gmail.com>
2020-10-08 15:36:37 -0600

    Used enumerate instead of range(len())

--- problems/p023.py
+++ problems/p023.py
@@ -16,10 +16,10 @@ class NonAbundantSums(Problem):
         is_sum_of_two = [False] * (limit + 1)
 
         # Loop through all combos and if the sum is greater then the limit then that sum can be set in lookup array
-        for i in range(len(abundants)):
+        for i, abundant in enumerate(abundants):
             for j in range(i, len(abundants)):
-                if abundants[i] + abundants[j] <= limit:
-                    is_sum_of_two[abundants[i] + abundants[j]] = True
+                if abundant + abundants[j] <= limit:
+                    is_sum_of_two[abundant + abundants[j]] = True
 
         # Return the sum of all numbers that arnt the sum of two abundant nums
         return sum(index for index, value in enumerate(is_sum_of_two) if not value)
