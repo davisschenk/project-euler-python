d9f8dcd5d6a8ea18aa90fddc54cda7050dd61e2c
Davis Schenkenberger <davisschenk@gmail.com>
2020-10-08 15:37:36 -0600

    Loop through list instead of indexes

--- problems/p031.py
+++ problems/p031.py
@@ -11,8 +11,8 @@ class CoinSum(Problem):
         dp = [0 for i in range(total + 1)]
         dp[0] = 1
 
-        for i in range(len(coins)):
-            for j in range(coins[i], total + 1):
-                dp[j] += dp[j - coins[i]]
+        for coin in coins:
+            for j in range(coin, total + 1):
+                dp[j] += dp[j - coin]
 
         return dp[total]
