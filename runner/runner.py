573a06e5bdec4db722929555e85279844721ee0d
Davis Schenkenberger <davisschenk@gmail.com>
2020-11-11 01:05:47 -0700

    worked on problem runner

---
+++ runner/runner.py
@@ -0,0 +1,40 @@
+import time
+from concurrent.futures import ProcessPoolExecutor
+from contextlib import redirect_stdout
+from io import StringIO
+
+from problem import Problem
+
+
+class Runner:
+    @staticmethod
+    def profile_problem(problem: Problem):
+        out = {"problem": problem.name, "solutions": []}
+        for solution in problem.solutions:
+            stdout = StringIO()
+            try:
+                with redirect_stdout(stdout):
+                    start = time.perf_counter()
+                    output = solution()
+                    end = time.perf_counter()
+            except Exception as exc:
+                out["solutions"].append({"solution": solution.name, "error": exc})
+
+            out["solutions"].append({
+                "solution": solution.name,
+                "time_seconds": end - start,
+                "output": output,
+                "stdout": stdout.read()
+            })
+
+        return out
+
+    @classmethod
+    def profile_problems(cls):
+        with ProcessPoolExecutor(max_workers=10) as p:
+            for output in p.map(cls.profile_problem, Problem.all_problems):
+                print(output)
+
+
+if __name__ == '__main__':
+    Runner.profile_problems()
